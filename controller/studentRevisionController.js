c5554b3b5483bdca310d282b1770473523795819
Enverd <yuvensenverd@yahoo.com>
2019-10-24 17:02:22 +0700

    BESOK KERJA BAGIAN YANG ADMIN UPDATE APPROVE

--- controller/studentRevisionController.js
+++ controller/studentRevisionController.js
@@ -33,72 +33,87 @@ module.exports = {
 
    
     postStudentRevision : (req,res) =>{
-        try {
-            console.log('upload')
-            const path = '/student/images'; //file save path
-            const upload = uploader(path, 'STD').fields([{ name: 'image'}]);
-            upload(req,res,(err)=>{
-                if(err){
-                    return res.status(500).json({ message: 'Upload picture failed !', error: err.message });
-                }
-                const {image}=req.files;
-                console.log(image)
-                const imagePath = image ? path + '/' + image[0].filename : null;
-                console.log(imagePath)
-                const data = JSON.parse(req.body.data);
-                console.log(data)
-                data.studentImage=imagePath
-                // data.tanggalLahir=Moment(data.tanggalLahir)
-                const {name,pendidikanTerakhir,gender,status,alamat,tanggalLahir,userId,story,schoolId,studentImage}=data
-                console.log(name)
-                return sequelize.transaction(function (t){
-                    return StudentRevision.create({
-                        name:name,
-                        pendidikanTerakhir:pendidikanTerakhir,
-                        gender:gender,
-                        status,
-                        alamat,
-                        tanggalLahir:Moment(tanggalLahir),
-                        userId,
+        const path = '/student/images'; //file save path
+        const upload = uploader(path, 'STD').fields([{ name: 'image'}]); //uploader(path, 'default prefix')
+
+        upload(req, res, (err) => {
+            if(err){
+                console.log("Masuk")
+                return res.status(500).json({ message: 'Upload picture failed !', error: err.message });
+            }
+
+            const { image } = req.files;
+            console.log(image)
+            const imagePath = image ? path + '/' + image[0].filename : null;
+            console.log(imagePath)
+      
+            console.log(req.body.data)
+            const data = JSON.parse(req.body.data);
+            console.log(data)
+            data.result[1].studentImage = imagePath
+       
+            // if(data.changeImage){
+            //     fs.unlinkSync('./public' + data.oldimg);
+            // }
+           
+            const {name,pendidikanTerakhir,gender,status,alamat,tanggalLahir,userId,story,schoolId,studentImage}=data.result[0]
+            console.log(name)
+            return sequelize.transaction(function (t){
+                return StudentRevision.create({
+                    name:name,
+                    pendidikanTerakhir:pendidikanTerakhir,
+                    gender:gender,
+                    status,
+                    alamat,
+                    tanggalLahir:Moment(tanggalLahir),
+                    userId,
+                    story,
+                    schoolId,
+                    studentImage
+                },{transaction:t})
+                .then((result)=>{
+                    console.log('insert student transaction success')
+                    
+                    const {pendidikanTerakhir, story, alamat, status, schoolId, id, studentImage} = data.result[1]
+                    return Student.update({
+                        pendidikanTerakhir,
                         story,
+                        alamat,
+                        status,
                         schoolId,
-                        studentImage
-                    },{transaction:t})
-                    .then((result)=>{
-                        return result
+                        studentImage,
+                        dataStatus : 'Update Unverified'
+                    }, {
+                        where : {
+                            id : id
+                        }
+                    }, {transaction : t}).then((result2)=>{
+                        console.log('berhasuil')
+
+                        // return res.status(200).send({message : 'success'})
                     }).catch((err)=>{
-                        console.log(err.message)
-                        fs.unlinkSync('./public' + imagePath);
-                        return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
+                        console.log('err1')
+                        throw new Error();
+                        // return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
                     })
-                }).then((result)=>{
-                    // console.log('success upload')
-                    // StudentRevision.findAll({
-                    //     attributes:{
-                    //         exclude:['createdAt','updatedAt']
-                    //     },
-                    //     where:{
-                    //         isDeleted:0
-                    //     }
-                    // })
-                    // .then((result1)=>{
-                    //     return res.status(200).send(result1)
-                    // }).catch((err)=>{
-                    //     res.status(500).send({message:'error post', error:err})
-                    // })
-                    return res.status(200).send(result)
-                }).catch((err)=>{
-                    console.log(err.message)
-                    fs.unlinkSync('./public' + imagePath);
-                    return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
-                })
-            })
             
-        } catch (error) {
-            console.log(err.message)
-            fs.unlinkSync('./public' + imagePath);
-            return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message });
-        }
+                }).catch((err)=>{
+                    console.log('err2')
+                    throw new Error();
+                    // return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
+                })
+            }).then((result)=>{
+                console.log('masukmasuk,asuk')
+                return res.status(200).send(result)
+            }).catch((err)=>{
+                console.log(err.message)
+                return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
+            })
+
+        })
+    
+            
+        
     },
     newStudentApprove : (req,res) =>{
         console.log('student approve')
