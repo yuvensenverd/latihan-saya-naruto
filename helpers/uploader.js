5c15ac49eee1b09c5fa9bfb9ee94e3b6d58ab140
mister-ard97 <rezardiansyah1997@gmail.com>
2020-07-20 09:38:03 +0700

    update_fileSize

--- helpers/uploader.js
+++ helpers/uploader.js
@@ -1,5 +1,5 @@
-const multer = require('multer');
-const fs = require('fs');
+const multer = require("multer");
+const fs = require("fs");
 
 //Convert the file size to megabytes (optional)
 // var fileSizeInMegabytes = fileSizeInBytes / 1000000.0
@@ -7,43 +7,46 @@ const fs = require('fs');
 // Return multer object
 
 module.exports = {
-    uploader(destination, fileNamePrefix){
-        let defaultPath = './public';
-        
-        const storage = multer.diskStorage({
-            destination: (req, file, cb) => {
-                const dir = defaultPath + destination;
-                if (fs.existsSync(dir)) {
-                    console.log(dir, "exists")
-                    cb(null, dir);
-                } else {
-                    fs.mkdir(dir, { recursive: true }, err => cb(err, dir));
-                    console.log(dir, "make")
-                }
-            },
-            filename: (req, file, cb) => {
-                console.log(file)
-                let originalname = file.originalname;
-                let ext = originalname.split('.');
-                let filename = fileNamePrefix + Date.now() + '.' + ext[ext.length - 1];
-                cb(null, filename);
-            }
-        });
-    
-        const imageFilter = (req, file, callback) => {
-            const ext = /\.(jpg|jpeg|png|mp4)$/;
-            if (!file.originalname.toLowerCase().match(ext)) {
-                return callback(new Error('Only selected file type are allowed'), false);
-            }
-            callback(null, true);
-        };
+  uploader(destination, fileNamePrefix) {
+    let defaultPath = "./public";
 
-        return multer({
-            storage: storage,
-            fileFilter: imageFilter,
-            limits : {
-                fileSize :  1024 * 1024 * 1024 // maximum of 90 MB File
-            }
-        });
-    }
-}
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        const dir = defaultPath + destination;
+        if (fs.existsSync(dir)) {
+          console.log(dir, "exists");
+          cb(null, dir);
+        } else {
+          fs.mkdir(dir, { recursive: true }, (err) => cb(err, dir));
+          console.log(dir, "make");
+        }
+      },
+      filename: (req, file, cb) => {
+        console.log(file);
+        let originalname = file.originalname;
+        let ext = originalname.split(".");
+        let filename = fileNamePrefix + Date.now() + "." + ext[ext.length - 1];
+        cb(null, filename);
+      },
+    });
+
+    const imageFilter = (req, file, callback) => {
+      const ext = /\.(jpg|jpeg|png|mp4)$/;
+      if (!file.originalname.toLowerCase().match(ext)) {
+        return callback(
+          new Error("Only selected file type are allowed"),
+          false
+        );
+      }
+      callback(null, true);
+    };
+
+    return multer({
+      storage: storage,
+      fileFilter: imageFilter,
+      limits: {
+        fileSize: 1024 * 1024 * 1024 * 1024, // maximum of 90 MB File
+      },
+    });
+  },
+};
